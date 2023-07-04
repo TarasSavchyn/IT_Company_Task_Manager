be8d0d3315c0c9a324334b96e019d45bab8ada82
Taras Savchyn <savik1992@gmail.com>
2023-07-04 22:35:17 +0300

    connect to postgresql DB Task Manager

--- it_company_task_manager/settings.py
+++ it_company_task_manager/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+import dotenv
+dotenv.load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,6 +93,9 @@ DATABASES = {
     }
 }
 
+# dj=database-url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
